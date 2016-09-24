68ff2e23fdaef2a25ae25fb2320badb6bd2a60f3
Emil <em111en@yandex.ru>
2016-09-25 01:29:27 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,20 @@
+ $(document).ready(function() {
+        
+     $('#main-page-slaider').coinslider({ //плагин на главной
+        width: 1920,
+        height:600,
+        spw:7,
+        sph:5,
+        delay:6000,
+        sDelay:30,
+        opacity:"0",
+        titleSpeed:500,
+        animation:"straight",
+        showNavigationButtons:true,
+        showNavigationPrevNext:false,
+        hoverPause:false,
+        prevText:false,
+        nextText:false
+    });
+  });
+  
